3325385e6a7af8ef19d51f0eb1db712be2933a62
Phoenix <anmolkhanna2003@gmail.com>
2024-04-24 19:18:35 +0530

    Initial commit

---
+++ src/main/java/com/example/basicWebApp/domain/Author.java
@@ -0,0 +1,48 @@
+package com.example.basicWebApp.domain;
+
+import jakarta.persistence.*;
+
+import java.util.*;
+
+@Entity
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    @ManyToMany(mappedBy ="authors")
+    private Set<Book> books;
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
